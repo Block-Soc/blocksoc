0e371eb37819d4452627024ee0190ca27523ba46
Kevin Mitchell <kevinmitch14@gmail.com>
2022-06-01 10:20:35 +0100

    Font optimisation

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 const HeroSection = () => {
     return (
         <div className="p-4 py-8 pt-4 mx-auto max-w-5xl" id="home-section">
-            <h1 className="text-5xl leading-tight md:text-[6rem] font-bold tracking-tighter text-white">
+            <h1 className="text-5xl leading-tight md:text-[6rem] font-extrabold tracking-tighter text-white">
                 A DAO built by students,<span className="text-indigo-500"> for students.</span>
             </h1>
             <div className="text-zinc-200 text-lg md:text-3xl md:leading-normal pt-4 tracking-normal">
